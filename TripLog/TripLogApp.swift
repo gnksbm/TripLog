4d1edd969d70229dd98025d1cf2cbb19ad0cc7fe
Geonseob Kim <gnksbm@gmail.com>
2024-09-10 22:07:49 +0900

    Initial Commit

---
+++ TripLog/TripLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripLogApp.swift
+//  TripLog
+//
+//  Created by gnksbm on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TripLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
