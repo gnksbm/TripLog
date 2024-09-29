7dd97bf117af62123203b5f2ee31878c974f7598
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-09-29 23:27:53 +0900

    [Add] 새로운 컬러셋 추가

--- TripLog/Sources/Foundation/TLDesign.swift
+++ TripLog/Sources/Foundation/TLDesign.swift
@@ -18,14 +18,40 @@ enum TLFont {
 enum TLColor {
     static let coralOrange = Color.coralOrange
     static let lightPeach = Color.lightPeach
-    static let royalBlue = Color.royalBlue
-    static let backgroundGray = Color.backgroundGray
-    static let separatorGray = Color.separatorGray
     
-    static let primaryText = Color.primaryText
-    static let secondaryText = Color.secondaryText
+    static let skyBlueLight = Color(hex: "CAF0F8")
+    static let skyBlueMedium = Color(hex: "ADE8F4")
+    static let skyBlue = Color(hex: "90E0EF")
+    static let oceanBlueLight = Color(hex: "48CAE4")
+    static let oceanBlue = Color(hex: "00B4D8")
+    static let deepBlue = Color(hex: "0096C7")
+    static let royalBlue = Color(hex: "0077B6")
+    static let navyBlue = Color(hex: "023E8A")
+    static let midnightBlue = Color(hex: "03045E")
     
-    static let successGreen = Color.successGreen
-    static let warningYellow = Color.warningYellow
-    static let errorRed = Color.errorRed
+    static let primaryText = Color(hex: "03045E")
+    static let secondaryText = Color(hex: "0077B6")
+    
+    static let successGreen = Color.green
+    static let warningYellow = Color.yellow
+    static let errorRed = Color.red
+    
+    static let backgroundGray = Color(UIColor.systemGray6)
+    static let separatorGray = Color(UIColor.systemGray4)
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.currentIndex = hex.startIndex
+        
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        
+        let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgbValue & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
 }
